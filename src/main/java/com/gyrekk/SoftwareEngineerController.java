64ccd60435e84f1c72f76c3b5184ccf64294f58b
Kamil Garcarek <gyrekk.business@gmail.com>
2025-11-23 20:57:09 +0100

    kk

---
+++ src/main/java/com/gyrekk/SoftwareEngineerController.java
@@ -0,0 +1,33 @@
+package com.gyrekk;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerId(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+
+
+}
---
+++ src/main/java/com/gyrekk/SoftwareEngineerRepository.java
@@ -0,0 +1,8 @@
+package com.gyrekk;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository
+        extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
